1fbbca151fe748e3854673b286ee4bca798f075d
Tony West <125916820+twest-bf@users.noreply.github.com>
2026-07-01 18:23:40 -0400

    Incremented version string

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 	"github.com/BishopFox/joro/internal/xsshunter"
 )
 
-var version = "v1.2.0"
+var version = "v1.3.0"
 var commit = "dev" // injected via -ldflags at build time
 
 func main() {
